2dd7101c2a27489831f9cd0883ce6fa331831a5e
Artem Zaytsev <artemzaitsev014@gmail.com>
2022-12-01 17:06:51 +0300

    fdfd

--- New/Node.php
+++ New/Node.php
@@ -2,7 +2,7 @@
 header("Content-Type: application/json; encoding=utf-8");
 
 $response['response'] = array(
-'item_id' => 125,
+'item_id' => 'sale_item_1',
 'title' => '300',
 'photo_url' => 'http://www.gameduel.16mb.com/coin.jpg',
 'price' => 5
